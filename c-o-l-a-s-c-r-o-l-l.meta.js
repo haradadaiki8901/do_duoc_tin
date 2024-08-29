3cdb48ec33d8dac4ec162090d2c6c01a90611125
haradadaiki8901 <haradadaiki8901@github.com>
2024-08-29 10:37:25 +0700

    up

--- c-o-l-a-s-c-r-o-l-l.meta.js
+++ c-o-l-a-s-c-r-o-l-l.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         cola scroll
 // @namespace    https://github.com/haradadaiki8901/do_duoc_tin/raw/main/c-o-l-a-s-c-r-o-l-l.user.js
-// @version      2024.08.13
+// @version      2024.08.29
 // @description  scroll download cola
 // @author       You
 // @icon         https://raw.githubusercontent.com/haradadaiki8901/do_duoc_tin/main/c-o-l-a-s-c-r-o-l-l.png
